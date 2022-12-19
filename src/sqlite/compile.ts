885f50dcd3ac4c8420e05c71339f78ddc693dc09
Andrew Kaiser <andrew.kaiser@wbgames.com>
2022-12-19 17:30:34 -0500

    move deps/ folder into src/

--- src/sqlite/compile.ts
+++ src/sqlite/compile.ts
@@ -1,12 +1,13 @@
 import * as path from 'https://deno.land/std@0.152.0/path/mod.ts'
-import { SQLiteTarget } from '../src/binary_manager.ts'
+import { SQLiteTarget } from '../binary_manager.ts'
 
 
-const deps_folder = path.dirname(path.fromFileUrl(import.meta.url))
-const src_folder = path.join(path.dirname(deps_folder), 'src')
-const dest_folder = path.join(path.dirname(deps_folder), 'binaries')
-const SQLITE_SOURCE = path.join(deps_folder, 'sqlite-source')
-const BUILD_FOLDER = `${deps_folder}/build`
+const sqlite_folder = path.dirname(path.fromFileUrl(import.meta.url))
+const root_folder = path.dirname(path.dirname(sqlite_folder))
+const src_folder = path.join(root_folder, 'src')
+const dest_folder = path.join(root_folder, 'binaries')
+const SQLITE_SOURCE = path.join(sqlite_folder, 'sqlite-source')
+const BUILD_FOLDER = `${sqlite_folder}/build`
 await Deno.mkdir(BUILD_FOLDER, { recursive: true })
 
 async function exec(cmd: string) {
@@ -18,10 +19,10 @@ async function exec(cmd: string) {
 }
 
 async function build_embedded_binary(arch: string, filename: string) {
-  const filepath = path.join(src_folder, 'binaries', `${arch}.ts`)
-  const relative_path = path.relative(path.dirname(deps_folder), filepath)
+  const filepath = path.join(root_folder, 'binaries', `${arch}.ts`)
+  const relative_path = path.relative(path.dirname(root_folder), filepath)
   console.log(`encoding binary into js file ${relative_path}...`)
-  const file_buffer = await Deno.readFile(path.join('deps','build', filename))
+  const file_buffer = await Deno.readFile(path.join(BUILD_FOLDER, filename))
   const embedded_binary_file_contents = `export default new Uint8Array([${file_buffer.join(',')}])`
   await Deno.writeTextFile(filepath, embedded_binary_file_contents)
 }
@@ -30,7 +31,7 @@ async function copy_binary(arch: string, filename: string) {
   const sqlite_target = await SQLiteTarget.create()
   if (sqlite_target.build.os !== arch) throw new Error(`Unexpected os ${arch} when building for ${arch}`)
   const ext = path.extname(filename)
-  const src_filepath = path.join('deps', 'build', filename)
+  const src_filepath = path.join(BUILD_FOLDER, filename)
   await Deno.mkdir(path.dirname(sqlite_target.filepath), { recursive: true })
   const dest_filepath = sqlite_target.filepath
   console.log(`copying binary to binaries/${sqlite_target.filename}...`)
